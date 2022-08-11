b475460d2bb0bfa07c1a5b3f1827ccfdfc041a65
Artus U <artusrock@hotmail.com>
2022-08-11 15:28:02 +0100

    image field added

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,9 @@
+from io import BytesIO
+from django.core.files import File
+from django.db import models
+from PIL import Image
+from django.contrib.auth.models import User 
+
 from django.db import models
 
 class Category(models.Model):
@@ -25,10 +31,29 @@ class Product(models.Model):
     num_visits = models.IntegerField(default=0)
     last_visit = models.DateTimeField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="uploads/", blank=True, null=True)
 
     class Meta:
         ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.image:
+            self.thumbnail = self.make_thumbnail(self.image)
+        super().save(*args, **kwargs)
+    
+    def make_thumbnail(self, image, size=(300, 200)) -> thumbnail:
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+        
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quantity=85)
+        
+        thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
         
