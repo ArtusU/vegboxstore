ecc147dd4c2fbffcb87cababc7aabe28b5d36a0d
Artus U <artusrock@hotmail.com>
2022-09-06 13:03:57 +0100

    Stripe webhook

---
+++ apps/cart/webhook.py
@@ -0,0 +1,29 @@
+import json
+import stripe
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from apps.order.models import Order
+
+
+@csrf_exempt
+def webhook(request):
+    payload = request.body
+    event = None
+    
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+    try:
+        event = stripe.Event.construct_from(json.loads(payload), stripe.api_key)
+    except ValueError as e:
+        return HttpResponse(status=404)
+    
+    if event.type == 'payment_intent.succeeded':
+        payment_intent = event.data.object    
+        order = Order.objects.get(payment_intent=payment_intent.id)
+        order.paid_amount = payment_intent.amount
+        order.paid = True
+        order.save()
+    return HttpResponse(status=200)
--- boxstore/urls.py
+++ boxstore/urls.py
@@ -4,16 +4,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.cart.webhook import webhook
 from apps.cart.views import cart_detail, success
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
-from apps.store.api import api_add_to_cart, api_remove_from_cart, api_checkout, checkout_session
+from apps.store.api import api_add_to_cart, api_remove_from_cart, checkout_session
 
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', frontpage, name='frontpage'),
     path('cart/', cart_detail, name='cart_detail'),
+    path('hooks/', webhook, name='webhook'),
     path('cart/success/', success, name='success'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
@@ -21,7 +23,6 @@ urlpatterns = [
     
     path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
     path('api/remove_from_cart/', api_remove_from_cart, name='api_remove_from_cart'),
-    path('api/checkout/', api_checkout, name='api_checkout'),
     path('api/checkout/session/', checkout_session, name='create_checkout_session'),
     
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
