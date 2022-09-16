8cd8a2e9158dba85e880ea96e779bfc99a9e9813
Artus U <artusrock@hotmail.com>
2022-09-16 13:47:14 +0100

    ProductImage model

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,4 +1,3 @@
-from audioop import reverse
 from io import BytesIO
 from django.core.files import File
 from django.db import models
@@ -62,5 +61,27 @@ class Product(models.Model):
         img.save(thumb_io, 'JPEG', quantity=85)
         
         thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
+    
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        self.thumbnail = self.make_thumbnail(self.image)
+        super().save(*args, **kwargs)
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
         return thumbnail
         
