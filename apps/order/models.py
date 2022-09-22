616414987619e2bea8697a3b89b9eb1bb0e0323e
Artus U <artusrock@hotmail.com>
2022-09-22 10:57:22 +0100

    get_total_quantity()

--- apps/order/models.py
+++ apps/order/models.py
@@ -40,6 +40,9 @@ class Order(models.Model):
     def __str__(self):
         return self.first_name
     
+    def get_total_quantity(self):
+        return sum(int(item.quantity) for item in self.items.all())
+    
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
