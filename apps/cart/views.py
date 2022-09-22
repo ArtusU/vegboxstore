edb54067fb199be237837679d41e4c77a456d545
Artus U <artusrock@hotmail.com>
2022-09-22 08:28:30 +0100

    prefiling user details to address form

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -12,9 +12,19 @@ def cart_detail(request):
         b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s', 'thumbnail': '%s', 'product_url': '%s', 'num_available': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'], product.thumbnail.url, product_url, product.num_available)
 
         productsstring = productsstring + b
+        
+    if request.user.is_authenticated:
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        email = request.user.email
+    else:
+        first_name = last_name = email = ''
     
     context = {
         'cart': cart,
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
         'pub_key': settings.STRIPE_PUBLISHABLE_KEY,
         'productsstring': productsstring.rstrip(',')
         }
