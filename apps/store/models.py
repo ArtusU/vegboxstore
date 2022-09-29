fcf5ad5bee7334ccc42c4b8b710fcdd7dbed669c
Artus U <artusrock@hotmail.com>
2022-09-29 12:10:57 +0100

    ProductReview Model

--- apps/store/models.py
+++ apps/store/models.py
@@ -85,4 +85,12 @@ class ProductImage(models.Model):
         thumbnail = File(thumb_io, name=image.name)
 
         return thumbnail
+    
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    stars = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
         
