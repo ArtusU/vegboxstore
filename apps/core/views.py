a3d130b2a6c711fc5acfcf2ca47074f33528c3c8
Artus U <artusrock@hotmail.com>
2022-10-06 11:05:19 +0100

    refactoring frontpage.html

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
-from apps.store.models import Product
+from apps.store.models import Category, Product
 from apps.order.models import Order
 
 
 def frontpage(request):
     products = Product.objects.filter(is_featured=True)
-    context = {'list_products': products}
+    featured_categories = Category.objects.filter(is_featured=True)
+    context = {'list_products': products, 'featured_categories': featured_categories,}
     return render(request, 'frontpage.html', context)
 
 
