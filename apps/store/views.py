e45c020f59d52770d9221228dff40d8a51302e47
Artus U <artusrock@hotmail.com>
2022-10-12 12:28:02 +0100

    sorting price high-low

--- apps/store/views.py
+++ apps/store/views.py
@@ -12,18 +12,25 @@ def search(request):
     instock = request.GET.get("instock")
     price_from = request.GET.get("price_from", 0) or 0
     price_to = request.GET.get("price_to", 1000000) or 10000000
-    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
-    print(price_to)
+    sorting = request.GET.get("sorting", "-date_added")
+    products = (
+        Product.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+        .filter(price__gte=price_from)
+        .filter(price__lte=price_to)
+    )
 
     if instock:
         products = products.filter(num_available__gte=1)
 
     context = {
         "query": query,
-        "products": products,
+        "products": products.order_by(sorting),
         "instock": instock,
         "price_from": price_from,
         "price_to": price_to,
+        "sorting": sorting,
     }
 
     return render(request, "search.html", context)
