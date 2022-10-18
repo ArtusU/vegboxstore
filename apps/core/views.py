d596e48182d69528869b6945d683670bd6c5fb2f
Artus U <artusrock@hotmail.com>
2022-10-18 09:08:38 +0100

    frontpage.html popular and recently visited prod

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,13 +7,20 @@ from apps.order.models import Order
 def frontpage(request):
     products = Product.objects.filter(is_featured=True)
     featured_categories = Category.objects.filter(is_featured=True)
-    context = {'list_products': products, 'featured_categories': featured_categories,}
-    return render(request, 'frontpage.html', context)
+    popular_products = Product.objects.all().order_by("-num_visits")[:4]
+    recently_viewed_products = Product.objects.all().order_by("-last_visit")[:4]
+    context = {
+        "list_products": products,
+        "featured_categories": featured_categories,
+        "popular_products": popular_products,
+        "recently_viewed_products": recently_viewed_products,
+    }
+    return render(request, "frontpage.html", context)
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
